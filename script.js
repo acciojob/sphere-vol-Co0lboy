2b307321edf980f0aa8d30fee92dc69fa7a0dabe
Aniket Pawar <115694154+Co0lboy@users.noreply.github.com>
2025-09-29 07:46:13 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
-function volume_sphere() {
+function volume_sphere(event) {
     //Write your code here
+	event.preventDefault();
+
+	const radius=parseFloat(document.getElementById('radius').value);
+	if(isNaN(radius)||radius<0){
+		alert("Please enter a valid non-negative number.");
+		document.getElementById('volume').value='';
+		return;
+	}
+	const volume=4/3*Math.PI*Math.pow(radius,3);
+	document.getElementById('volume').value=volume.toFixed(4);
   
 } 
 
